a5c66522d055efa7fd86660c5ea948ee99ac731d
aman <sagaraman456@gmail.com>
2022-10-15 18:30:33 +0530

    cplusplus basics

---
+++ C++/tut3.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+//code with harry
+/* this is a multi
+line comment*/
+int main()
+{
+    int sum = 6;
+    cout << "hello world\n"<<sum<<endl;
+    return 0;
+}
---
+++ C++/tut4.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int glo = 8;
+void sum()
+{
+    int a=6;
+    cout << glo;
+}
+int main()
+{
+    int a = 16;
+    int b = 4;
+    float pi = 3.14;
+    char c = 'u';
+    int glo = 10;
+    glo = -9;
+    sum();
+    // cout << glo << endl;
+
+    return 0;
+}
